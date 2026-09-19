8f75feaa12a2345e64e6e0edba86d7692e726560
Kris Scott <kscott91@gmail.com>
2026-09-19 14:55:48 -0700

    Prevent clipboard-offer crashes from VNC client disconnects

--- overlay.nix
+++ overlay.nix
@@ -1,7 +1,17 @@
 final: prev: {
+  wayvnc = prev.wayvnc.overrideAttrs (old: {
+    # https://github.com/any1/wayvnc/pull/456
+    patches = (old.patches or [ ]) ++ [
+      (prev.fetchpatch {
+        url = "https://github.com/krscott/wayvnc/commit/a567db5577f3303e14cbde8af25faf47fc9fae3f.patch";
+        hash = "sha256-FF0XRizwGNk67zAQd0X5Fe1c2JBzzOpXGi0Wad4Juak=";
+      })
+    ];
+  });
+
   python3 = prev.python3.override {
     packageOverrides = _: _: {
-      framewisp = prev.python3.pkgs.callPackage ./default.nix { };
+      framewisp = prev.python3.pkgs.callPackage ./default.nix { inherit (final) wayvnc; };
     };
   };
 
